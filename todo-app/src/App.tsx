84f280af181dc341fee51c8fa8895b1b9fdc1e15
Ange290 <uange209@gmail.com>
2026-04-09 20:39:43 +0200

    Fix todo list

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -3,24 +3,32 @@ import CreateTodo from './components/CreateTodo';
 import TodoList from './components/TodoList';
 import './App.css'
 
-interface TodoItem {
+export interface TodoItem {
   id: string;
   title: string;
   completed: boolean;
 }
 
 function App() {
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [todos, setTodos] = useState<TodoItem[]>([
+    { id: '1', title: 'Sample Todo', completed: false }
+  ]);
 
   const handleAddTodo = (newTodo: TodoItem) => {
     setTodos([...todos, newTodo]);
+  };
+
+  const handleToggleTodo = (id: string) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
   };
 
   return (
     <>
       <h1 className='text-center text-gray-200 text-9xl py-5'>todos</h1>
       <CreateTodo onAddTodo={handleAddTodo} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggleTodo={handleToggleTodo} />
     </>
   )
 }
